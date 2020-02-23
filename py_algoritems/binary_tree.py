5034c3f140ba83d7e71803ee2ab0e2b1098d87bb
adibl <bleyer23@gmail.com>
2020-02-23 17:54:13 +0200

    fix right left problem

--- py_algoritems/binary_tree.py
+++ py_algoritems/binary_tree.py
@@ -6,8 +6,8 @@ class TreeNode(object):
     def __init__(self, value, parent=None):
         self.value = value
         self._parent = parent
-        self._right_child = None
         self._left_child = None
+        self._right_child = None
 
     def __eq__(self, other):
         if type(other) is TreeNode:
@@ -26,10 +26,10 @@ class TreeNode(object):
             raise NotImplementedError
 
     def __delete__(self):
-        if self is self.parent.right_child:
-            self.parent.right_child = None
-        elif self is self.parent.left_child:
+        if self is self.parent.left_child:
             self.parent.left_child = None
+        elif self is self.parent.right_child:
+            self.parent.right_child = None
         self.parent = None
 
     @property
@@ -39,26 +39,16 @@ class TreeNode(object):
     @parent.setter
     def parent(self, value):
         if self._parent is not None:
-            if self._parent.right_child is self:
-                self._parent.right_child = None
-            elif self._parent.left_child is self:
+            if self._parent.left_child is self:
                 self._parent.left_child = None
+            elif self._parent.right_child is self:
+                self._parent.right_child = None
         self._parent = value
         if self._parent is not None:
             if self > self._parent:
-                self._parent.left_child = self
-            else:
                 self._parent.right_child = self
-
-    @property
-    def right_child(self):
-        return self._right_child
-
-    @right_child.setter
-    def right_child(self, value):
-        self._right_child = value
-        if value is not None:
-            value._parent = self
+            else:
+                self._parent.left_child = self
 
     @property
     def left_child(self):
@@ -70,8 +60,18 @@ class TreeNode(object):
         if value is not None:
             value._parent = self
 
+    @property
+    def right_child(self):
+        return self._right_child
+
+    @right_child.setter
+    def right_child(self, value):
+        self._right_child = value
+        if value is not None:
+            value._parent = self
+
     def is_leaf(self):
-        return self.right_child is None and self.left_child is None
+        return self.left_child is None and self.right_child is None
 
 
 class BinaryTree(object):
@@ -80,15 +80,15 @@ class BinaryTree(object):
 
     def remove_leaf(self, node):
         if node.is_leaf():
-            if node._parent.left_child is node:
-                node._parent.left_child = None
-            elif node._parent.right_child is node:
+            if node._parent.right_child is node:
                 node._parent.right_child = None
+            elif node._parent.left_child is node:
+                node._parent.left_child = None
         else:
             raise NotImplementedError
 
     def right_ancestor(self, node):
-        while node < node._parent:
+        while node > node._parent:
             node = node._parent
         return node
 
@@ -105,8 +105,8 @@ class BinaryTree(object):
 
     def get_biggest(self):
         node = self.root
-        while node.left_child is not None:
-            node = node.left_child
+        while node.right_child is not None:
+            node = node.right_child
         return node
 
     def premote(self, node):
@@ -120,20 +120,20 @@ class BinaryTree(object):
         elif node2 is self.root:
             self.root = node
         node.parent, node2.parent = node2.parent, node.parent
-        node.right_child, node2.right_child = node2.right_child, node.right_child
         node.left_child, node2.left_child = node2.left_child, node.left_child
+        node.right_child, node2.right_child = node2.right_child, node.right_child
 
     def find(self, data):
         node = self.root
         while node is not None:
             if data > node:
-                if node.left_child is None:
-                    return node
-                node = node.left_child
-            elif data < node:
                 if node.right_child is None:
                     return node
                 node = node.right_child
+            elif data < node:
+                if node.left_child is None:
+                    return node
+                node = node.left_child
 
             else:
                 return node
@@ -146,31 +146,12 @@ class BinaryTree(object):
         else:
             place = self.find(data)
             if data > place:
-                place.left_child = TreeNode(data)
-                place = place.left_child
-            elif data < place:
                 place.right_child = TreeNode(data)
                 place = place.right_child
+            elif data < place:
+                place.left_child = TreeNode(data)
+                place = place.left_child
             return place
-
-    def delete(self, data):
-        if self.root is not None:
-            node_to_delete = self.find(data)
-            if node_to_delete.is_leaf():
-                self.remove_leaf(node_to_delete)
-            if node_to_delete.right_child is None:
-                node_to_delete.left_child.parent = node_to_delete.parent
-                ret = node_to_delete.value
-                del node_to_delete
-                return ret
-            else:
-                replace_node = self.next_in_size(node_to_delete)
-                self.switch(replace_node, node_to_delete)
-                if node_to_delete.right_child is not None:
-                    self.premote(node_to_delete.right_child)
-                ret = node_to_delete.value
-                self.remove_leaf(node_to_delete)
-                return ret
 
     def get_in_order(self):
         return self.get_in_order_recursion(self.root)
@@ -178,7 +159,7 @@ class BinaryTree(object):
     def get_in_order_recursion(self, root):
         if root is None:
             return []
-        return self.get_in_order_recursion(root.right_child) + [root.value, ] + self.get_in_order_recursion(root.left_child)
+        return self.get_in_order_recursion(root.left_child) + [root.value, ] + self.get_in_order_recursion(root.right_child)
 
     def get_pre_order(self):
         return self.get_pre_order_recursion(self.root)
@@ -186,7 +167,7 @@ class BinaryTree(object):
     def get_pre_order_recursion(self, root):
         if root is None:
             return []
-        return [root.value, ] + self.get_pre_order_recursion(root.right_child) + self.get_pre_order_recursion(root.left_child)
+        return [root.value, ] + self.get_pre_order_recursion(root.left_child) + self.get_pre_order_recursion(root.right_child)
 
     def get_post_order(self):
         return self.get_post_order_recursion(self.root)
@@ -194,23 +175,23 @@ class BinaryTree(object):
     def get_post_order_recursion(self, root):
         if root is None:
             return []
-        return self.get_post_order_recursion(root.right_child) + self.get_post_order_recursion(root.left_child) + [root.value, ]
+        return self.get_post_order_recursion(root.left_child) + self.get_post_order_recursion(root.right_child) + [root.value, ]
 
 
 def test_Node_parameters():
     n1 = TreeNode('fff')
     n2 = TreeNode('abc')
-    n1.right_child = n2
+    n1.left_child = n2
     assert n2.parent is n1
 
 
 def test_node_delete():
     n1 = TreeNode('1')
-    n1.right_child = TreeNode('2')
-    n1.right_child.right_child = TreeNode('3')
-    n2 = n1.right_child
+    n1.left_child = TreeNode('2')
+    n1.left_child.left_child = TreeNode('3')
+    n2 = n1.left_child
     del n2
-    n1.right_child is None
+    n1.left_child is None
 
 
 def test_compare():
@@ -225,7 +206,7 @@ def test_tree_add():
     tree.add(12)
     tree.add(5)
     tree.add(6)
-    assert tree.root.left_child is None
+    assert tree.root.right_child is None
 
 
 def test_tree_find():
@@ -244,42 +225,6 @@ def test_tree_find_not_precise():
     assert tree.find(14) == 15
     assert tree.find(11) == 7
     assert tree.find(4) == 7
-
-
-def test_tree_delete_near():
-    tree = BinaryTree()
-    tree.add(12)
-    tree.add(7)
-    tree.add(15)
-    tree.delete(12)
-    assert tree.find(7) == 7
-    assert tree.find(15) == 15
-    assert tree.find(12) == 15
-    assert tree.root == 7
-    assert tree.root.left_child == 15
-    assert tree.root.right_child is None
-
-
-def test_delete_far():
-    tree = BinaryTree()
-    tree.add(10)
-    tree.add(5)
-    tree.add(3)
-    tree.add(2)
-    tree.delete(5)
-    assert tree.find(5) == 3
-    assert tree.find(2) == 2
-
-
-def test_delete_far2():
-    tree = BinaryTree()
-    tree.add(2)
-    tree.add(3)
-    tree.add(5)
-    tree.add(10)
-    tree.delete(5)
-    assert tree.find(5) == 10
-    assert tree.find(2) == 2
 
 
 def test_get_in_order():
--- py_algoritems/splay_tree.py
+++ py_algoritems/splay_tree.py
@@ -9,11 +9,11 @@ class SplayTree(BinaryTree):
         biggest2 = tree2.get_biggest()
         if biggest1 > biggest2:
             tree2.splay(biggest2)
-            tree2.root.left_child = tree1.root
+            tree2.root.right_child = tree1.root
             return tree2
         else:
             tree1.splay(biggest1)
-            tree1.root.left_child = tree2.root
+            tree1.root.right_child = tree2.root
             return tree1
 
     def add_no_splay(self, vlaue):
@@ -25,35 +25,35 @@ class SplayTree(BinaryTree):
 
     def zig(self, node: TreeNode):
         parent = node.parent
-        is_left_zig = parent.left_child is node
-        node_left = node.left_child
+        is_right_zig = parent.right_child is node
         node_right = node.right_child
+        node_left = node.left_child
         if parent is self.root:
             self.root = node
         else:
             node.parent = parent.parent
-        if is_left_zig:
-            node.right_child = parent
-            node.left_child = node_left
-            parent.left_child = node_right
-        else:
-            node.right_child = node_right
+        if is_right_zig:
             node.left_child = parent
+            node.right_child = node_right
             parent.right_child = node_left
+        else:
+            node.left_child = node_left
+            node.right_child = parent
+            parent.left_child = node_right
 
     def zag(self, node):
         parent = node.parent
-        is_left_zag = parent.left_child is node
+        is_right_zag = parent.right_child is node
         if parent is self.root:
             self.root = node
         else:
             node.parent = parent.parent
-        if is_left_zag:
-            parent.left_child = node.right_child
-            node.right_child = parent
-        else:
+        if is_right_zag:
             parent.right_child = node.left_child
             node.left_child = parent
+        else:
+            parent.left_child = node.right_child
+            node.right_child = parent
 
     def zigzag(self, node):
         self.zig(node)
@@ -68,27 +68,12 @@ class SplayTree(BinaryTree):
             if self.root is node.parent:
                 self.zig(node)
             else:
-                is_node_left = node.parent.left_child is node
-                is_parent_left = node.parent.parent.left_child is node.parent
-                if is_node_left != is_parent_left:
+                is_node_right = node.parent.right_child is node
+                is_parent_right = node.parent.parent.right_child is node.parent
+                if is_node_right != is_parent_right:
                     self.zigzag(node)
                 else:
                     self.zigzig(node)
-
-
-def test_zig_right():
-    t = SplayTree()
-    t.add_no_splay(10)
-    t.add_no_splay(5)
-    t.add_no_splay(7)
-    t.add_no_splay(2)
-    t.add_no_splay(17)
-    t.zig(t.root.right_child)
-    assert t.root == 5
-    assert t.root.left_child == 10
-    assert t.root.left_child.left_child == 17
-    assert t.root.left_child.right_child == 7
-    assert t.root.right_child == 2
 
 
 def test_zig_left():
@@ -98,16 +83,31 @@ def test_zig_left():
     t.add_no_splay(7)
     t.add_no_splay(2)
     t.add_no_splay(17)
-    t.zig(t.root.right_child)
     t.zig(t.root.left_child)
-    assert t.root == 10
-    assert t.root.left_child == 17
-    assert t.root.right_child == 5
-    assert t.root.right_child.right_child == 2
+    assert t.root == 5
+    assert t.root.right_child == 10
+    assert t.root.right_child.right_child == 17
     assert t.root.right_child.left_child == 7
+    assert t.root.left_child == 2
 
 
-def test_zigzag_left():
+def test_zig_right():
+    t = SplayTree()
+    t.add_no_splay(10)
+    t.add_no_splay(5)
+    t.add_no_splay(7)
+    t.add_no_splay(2)
+    t.add_no_splay(17)
+    t.zig(t.root.left_child)
+    t.zig(t.root.right_child)
+    assert t.root == 10
+    assert t.root.right_child == 17
+    assert t.root.left_child == 5
+    assert t.root.left_child.left_child == 2
+    assert t.root.left_child.right_child == 7
+
+
+def test_zigzag_right():
     t = SplayTree()
     t.add_no_splay(10)
     t.add_no_splay(17)
@@ -116,14 +116,14 @@ def test_zigzag_left():
     t.add_no_splay(7)
     t.add_no_splay(6)
     t.add_no_splay(8)
-    t.zigzag(t.root.right_child.left_child)
+    t.zigzag(t.root.left_child.right_child)
     assert t.root == 7
-    assert t.root.right_child == 5
-    assert t.root.right_child.right_child == 4
-    assert t.root.right_child.left_child == 6
-    assert t.root.left_child == 10
-    assert t.root.left_child.right_child == 8
-    assert t.root.left_child.left_child == 17
+    assert t.root.left_child == 5
+    assert t.root.left_child.left_child == 4
+    assert t.root.left_child.right_child == 6
+    assert t.root.right_child == 10
+    assert t.root.right_child.left_child == 8
+    assert t.root.right_child.right_child == 17
 
 
 def test_splay():
@@ -132,7 +132,7 @@ def test_splay():
     t.add_no_splay(11)
     t.add_no_splay(12)
     t.add_no_splay(13)
-    t.splay(t.root.left_child.left_child.left_child)
+    t.splay(t.root.right_child.right_child.right_child)
     assert t.root == 13
 
 
@@ -145,9 +145,9 @@ def test_merge():
     t2.add_no_splay(3)
     t_result = SplayTree.merge(t1, t2)
     assert t_result.root == 3
-    assert t_result.root.right_child == 2
-    assert t_result.root.left_child == 5
-    assert t_result.root.left_child.left_child == 7
+    assert t_result.root.left_child == 2
+    assert t_result.root.right_child == 5
+    assert t_result.root.right_child.right_child == 7
 
 
 def test_delte():
@@ -157,8 +157,8 @@ def test_delte():
     t.add_no_splay(5)
     t.add_no_splay(4)
     t.add_no_splay(7)
-    t.delete(t.root.right_child.left_child)
+    t.delete(t.root.left_child.right_child)
     assert t.root == 5
-    assert t.root.left_child == 10
-    assert t.root.left_child.left_child == 17
-    assert t.root.right_child == 4
+    assert t.root.right_child == 10
+    assert t.root.right_child.right_child == 17
+    assert t.root.left_child == 4
